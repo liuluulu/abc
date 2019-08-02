07c2309ac51b84fec9963218d2d50de626a59e4e
刘璐 <@192.168.67.1>
2019-08-02 10:25:49 +0800

    1

---
+++ sp04-orderService/src/main/java/com/tedu/sp04/Sp04OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.sp04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp04OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp04OrderServiceApplication.class, args);
+	}
+
+}
